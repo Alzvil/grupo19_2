a1f604dfff62fd8c54eab4891fe15a9cc223220e
Alzvil <villena.alan@gmail.com>
2022-05-19 20:03:20 -0400

    Restauracion de routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'turnos/index'
+  get 'turnos/new'
+  get 'turnos/create'
+  get 'turnos/edit'
+  get 'turnos/update'
+  get 'turnos/delete'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
