25bb3f366808b119de6051f90224abd2548a1edc
Alzvil <villena.alan@gmail.com>
2022-05-19 20:55:23 -0400

    Solucion routes.rb para turnos

--- app/controllers/turnos_controller.rb
+++ app/controllers/turnos_controller.rb
@@ -1,11 +1,16 @@
 class TurnosController < ApplicationController
   def index
+    @turnos = Turno.all
   end
 
   def new
+    @turno = Turno.new
   end
 
   def create
+  end
+
+  def show
   end
 
   def edit
